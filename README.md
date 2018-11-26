fa782671e52a731529dafb10316e5095577fe028
TonyS <tonysilvasd@gmail.com>
2018-11-26 11:24:27 -0800

    add initial project files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hey There :wave:
+
+In this project, I am creating my backend for a TODO app.  I am able to see what TODO items are currently in the list, select individual items, delete individual items and post individual items.  If an item is posted with an existing TODO item id, the existing item is overwritten with the info in the post request.
+
+To get started, open the app in VS code, or code editor of your choosing, and install the dependencies via the terminal by typing '$ npm install'. Next, in the terminal, type '$ node server/index.js' to start the app on the local node server and open the localhost:8484 in the browser of your choice.  To see all TODO items, type *localhost:8484/api/TodoItems*. You can select individual items as well: *localhost:8484/api/TodoItems/'enter item number here'* ie '1'
+
+## ENJOY! :ok_hand:
--- server/app.js
+++ server/app.js
@@ -1,8 +1,72 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 const app = express();
+//const sampleData = require('./data');
+app.use(bodyParser.json());
+var mockData = [
+    {
+      todoItemId: 0,
+      name: 'an item',
+      priority: 3,
+      completed: false
+    },
+    {
+      todoItemId: 1,
+      name: 'another item',
+      priority: 2,
+      completed: false
+    },
+    {
+      todoItemId: 2,
+      name: 'a done item',
+      priority: 1,
+      completed: true
+    }
+];
 
 // add your code here
+app.get('/', (req, res) => {
+    console.log('data sent');
+    res.json('{status : ok}');
+    });
 
+app.get('/api/TodoItems', (req, res, next)=> {
+        console.log('sending the data your way!');
+        res.json(mockData);
+        next();
+    });
+
+app.post('/api/TodoItems', (req, res) => {
+    var newItem = {
+        todoItemId: req.body.todoItemId,
+        name: req.body.name,
+        priority: req.body.priority,
+        completed: req.body.completed
+    };
+    var found = mockData.find(o => o.todoItemId === newItem.todoItemId);
+    if (found === undefined){
+        mockData.push(newItem);
+    }
+    else {
+        console.log(found);
+        found.todoItemId = newItem.todoItemId,
+        found.name = newItem.name, 
+        found.priority =  newItem.priority,
+        found.completed = newItem.completed;
+        console.log(found);
+    }
+    res.status(201).json(newItem);   
+    });
+
+app.get('/api/TodoItems/:id', (req, res)=> {
+    console.log('sending the data your way!');
+    res.json((mockData[req.params.id]));
+    });
+
+app.delete('/api/Todoitems/:id', (req, res) => {
+    console.log('Can I delete something')
+    res.send(mockData[req.params.id]);
+    })    
+    
 module.exports = app;
